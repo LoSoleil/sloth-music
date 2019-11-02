1733942943d7467930490671237a5cef2fdf22d2
LoSoleil <2831211462@qq.com>
2019-11-02 17:24:51 +0800

    初步框架搭建

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,184 @@
+<template>
+  <div id="wrapper">
+    <el-container>
+      <el-header height="40px" style="text-align: right; font-size: 14px;background-color: salmon;">
+        <div style="margin-top: 5px;">
+          <i class="el-icon-arrow-left"></i>
+          <i class="el-icon-arrow-right"></i>
+          <i class="el-icon-refresh"></i>
+          <el-input
+            placeholder="请输入内容"
+            size="mini"
+            style="width: 158px; height: 10px; margin-right: 28%;"
+            prefix-icon="el-icon-search"
+            v-model="searchInput"
+          ></el-input>
+          <span>
+            <i class="el-icon-user"></i>
+          </span>
+          <span style="margin-right: 10px">登陆</span>
+          <span>
+            <i class="el-icon-chat-line-square" style="margin-right: 10px"></i>
+          </span>
+          <span>
+            <i class="el-icon-setting" style="margin-right: 10px"></i>
+          </span>
+          <span>
+            <i class="el-icon-copy-document"></i>
+          </span>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside>
+          <left-menu></left-menu>
+        </el-aside>
+        <el-container>
+          <el-main>
+            <div>
+              <el-carousel :interval="4000" type="card" height="200px">
+                <el-carousel-item v-for="item in bannerList" :key="item.id">
+                  <img :src='item.imgUrl'></img>
+                </el-carousel-item>
+              </el-carousel>
+            </div>
+          </el-main>
+        </el-container>
+      </el-container>
+      <el-footer></el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import LeftMenu from "./LeftMenu";
+
+export default {
+  name: "landing-page",
+  components: { LeftMenu },
+  data() {
+    return {
+      searchInput: "",
+      tableData: "",
+      bannerList:[
+      ]
+    };
+  },
+  methods: {
+    open(link) {
+      this.$electron.shell.openExternal(link);
+    }
+  },
+  mounted(){
+    this.bannerList = [
+        {
+          id: '1',
+        imgUrl: 'src/renderer/assets/img/banner1.jpg'
+      },
+      {
+        id: '2',
+        imgUrl: 'src/renderer/assets/img/banner2.jpg'
+      },
+      {
+        id: '3',
+        imgUrl: 'src/renderer/assets/img/banner3.jpg'
+      },
+      {
+        id: '4',
+        imgUrl: 'src/renderer/assets/img/banner4.jpg'
+      },
+      {
+        id: '5',
+        imgUrl: 'src/renderer/assets/img/banner5.jpg'
+      },
+      {
+        id: '6',
+        imgUrl: 'src/renderer/assets/img/banner6.jpg'
+      }
+    ]
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Source Sans Pro", sans-serif;
+}
+
+#wrapper {
+  background: radial-gradient(
+    ellipse at top left,
+    rgba(255, 255, 255, 1) 40%,
+    rgba(229, 229, 229, 0.9) 100%
+  );
+  height: 100vh;
+  width: 100vw;
+}
+
+#logo {
+  height: auto;
+  margin-bottom: 20px;
+  width: 420px;
+}
+
+main {
+  display: flex;
+  justify-content: space-between;
+}
+
+main > div {
+  flex-basis: 50%;
+}
+
+.left-side {
+  display: flex;
+  flex-direction: column;
+}
+
+.welcome {
+  color: #555;
+  font-size: 23px;
+  margin-bottom: 10px;
+}
+
+.title {
+  color: #2c3e50;
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 6px;
+}
+
+.title.alt {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.doc p {
+  color: black;
+  margin-bottom: 10px;
+}
+
+.doc button {
+  font-size: 0.8em;
+  cursor: pointer;
+  outline: none;
+  padding: 0.75em 2em;
+  border-radius: 2em;
+  display: inline-block;
+  color: #fff;
+  background-color: #4fc08d;
+  transition: all 0.15s ease;
+  box-sizing: border-box;
+  border: 1px solid #4fc08d;
+}
+
+.doc button.alt {
+  color: #42b983;
+  background-color: transparent;
+}
+</style>
---
+++ src/renderer/components/LeftMenu.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="leftContent">
+    <ul>
+      <li>
+        <span>在线音乐</span>
+        <i class="el-icon-chat-line-square"></i>
+        <span>反馈</span>
+      </li>
+      <li>
+        <i class="el-icon-magic-stick"></i>
+        <span>推荐</span>
+      </li>
+      <li>
+        <i class="el-icon-monitor"></i>
+        <span>电台</span>
+      </li>
+      <li>
+        <i class="el-icon-notebook-2"></i>
+        <span>歌单</span>
+      </li>
+      <li>
+        <i class="el-icon-trophy"></i>
+        <span>排行榜</span>
+      </li>
+      <li>
+        <i class="el-icon-coordinate"></i>
+        <span>歌手</span>
+      </li>
+      <li>
+        <i class="el-icon-mic"></i>
+        <span>mv</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style>
+li {
+  list-style: none;
+  padding-bottom: 10px;
+  font-size: 14px;
+}
+.leftContent {
+  background-color: gainsboro;
+  padding: 40px;
+}
+li:nth-child(1) {
+  font-size: 12px;
+  color: gray;
+}
+li:nth-child(1) > span:nth-child(1) {
+  padding-right: 60px;
+}
+</style>
